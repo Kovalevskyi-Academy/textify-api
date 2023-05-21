dfe9e58de3e13570ace15bb579dcfffce28621df
seralekseenko <seralekseenko@ukr.net>
2023-05-21 17:03:08 +0300

    fix test scripts

--- scripts/testNodes.sh
+++ scripts/testNodes.sh
@@ -4,34 +4,32 @@ set -Eeuo pipefail
 # get url from scripts args
 url=${1}
 
-
 # Do POST to REST-API /nodes
 actualUuid=null
 responseStatus="no"
 failAttempt=0
 waitTime=10
-while [ "${responseStatus}" != "201" ]
-do
+while [ "${responseStatus}" != "201" ]; do
   printf "\n#Do POST to %s/nodes\n" "${url}"
   echo "-> attempt # ${failAttempt}"
-#  printf "WORKDIR: %s" "$(ls ./*/)"
+  #  printf "WORKDIR: %s" "$(ls ./*/)"
   curl \
     --url "${url}/nodes" \
     --data @./testObjects/node1.json \
     -H "Content-Type: application/json" \
-    -w "%{http_code}" > responceNode.txt \
+    -w "%{http_code}" \
     -o nodeUUID.txt \
     -m "${waitTime}" \
-    -v || true #2>/dev/null
+    -v >responceNode.txt ||
+    true #2>/dev/null
   sleep "${waitTime}"s
   responseStatus=$(<responceNode.txt)
   printf "\n responseStatus: %s\n" "${responseStatus}"
-  if [ "${failAttempt}" -gt 10 ]
-  then
+  if [ "${failAttempt}" -gt 10 ]; then
     echo "POST node1 FAILED after ${failAttempt} !"
     exit 1
   fi
-  failAttempt=$(( "${failAttempt}" + 1))
+  failAttempt=$(("${failAttempt}" + 1))
 done
 printf "\nnode1 POST SUCCESS, UUID: %s\n" "$(tr <nodeUUID.txt -d \")"
 
@@ -39,24 +37,23 @@ printf "\nnode1 POST SUCCESS, UUID: %s\n" "$(tr <nodeUUID.txt -d \")"
 responseStatus="no"
 failAttempt=0
 waitTime=10
-while [ "${responseStatus}" != "200" ]
-do
+while [ "${responseStatus}" != "200" ]; do
   actualUuid=$(tr <nodeUUID.txt -d \")
   printf "\n#Do GET to %s/nodes/%s   → attempt#%s\n" "${url}" "${actualUuid}" "${failAttempt}"
   curl "${url}/nodes/${actualUuid}" -s \
     -o tempNode.json \
-    -w "%{http_code}" > responceNode.txt \
+    -w "%{http_code}" \
     -m "${waitTime}" \
-    -v || true
-    #2>/dev/null
+    -v >responceNode.txt ||
+    true
+  #2>/dev/null
   sleep "${waitTime}"s
   responseStatus=$(<responceNode.txt)
-  if [ "${failAttempt}" -gt 5 ]
-    then
-      echo "GET node1 FAILED after ${failAttempt} !"
-      exit 1
+  if [ "${failAttempt}" -gt 5 ]; then
+    echo "GET node1 FAILED after ${failAttempt} !"
+    exit 1
   fi
-  failAttempt=$(( "${failAttempt}" + 1))
+  failAttempt=$(("${failAttempt}" + 1))
 done
 
 echo "GET node1 SUCCESS after ${failAttempt} !"
--- scripts/testStories.sh
+++ scripts/testStories.sh
@@ -5,46 +5,58 @@ set -Eeuo pipefail
 url=${1}
 
 # Do POST to REST-API /nodes
-printf "\n#Do POST to %s/stories\n" "${url}"
 actualUuid=null
+responseStatus="no"
+failAttempt=0
+waitTime=10
+while [ "${responseStatus}" != "201" ]; do
+  printf "\n#Do POST to %s/stories\n" "${url}"
+  echo "-> attempt # ${failAttempt}"
+  curl \
+    --url "${url}/stories" \
+    -X POST \
+    -d @./testObjects/story1.json \
+    -H "Content-Type: application/json" \
+    -w "%{http_code}" \
+    --m "${waitTime}" \
+    -o storyUUID.txt \
+    -v >responceStory.txt ||
+    true #2>/dev/null
 
-curl "${url}/stories" \
-  -X POST \
-  -d @./testObjects/story1.json \
-  -H "Content-Type: application/json" \
-  -w "%{http_code}" > responce.txt \
-  -m 10 \
-  -o storyUUID.txt \
-  -v || true #2>/dev/null
+  responseStatus=$(<responceStory.txt)
+  printf "\n responseStatus: %s\n" "${responseStatus}"
+  if [ "${failAttempt}" -gt 10 ]; then
+    echo "POST story1 FAILED after ${failAttempt} !"
+    exit 1
+  fi
+  failAttempt=$(("${failAttempt}" + 1))
+done
 
-responseStatus=$(<responce.txt)
-if [ "${responseStatus}" = "201" ]
-then
-  printf "\nPOST 'story1' SUCCESS!\n"
-  actualUuid=$(tr <storyUUID.txt -d \")
-  echo "'story1' uuid is: ${actualUuid}"
-else
-  echo "POST 'story1' FAILED!"
-  printf "\nStory UUID: %s\n" "${actualUuid}"
-  printf "\nResponse STATUS: %s\n" "${responseStatus}"
-  exit 1
-fi
+printf "\nPOST 'story1' SUCCESS, UUID: %s\n" "$(tr <storyUUID.txt -d \")"
 
 # Check POSTed object -> do get
-printf "\n#Do GET to %s/stories/%s\n" "${url}" "${actualUuid}"
+responseStatus="no"
+failAttempt=0
+waitTime=10
+while [ "${responseStatus}" != "200" ]; do
+  actualUuid=$(tr <storyUUID.txt -d \")
+  printf "\n#Do GET to %s/stories/%s\n" "${url}" "${actualUuid}"
+  curl "${url}/stories/${actualUuid}" \
+    -o tempStory.json \
+    -w "%{http_code}" \
+    -m "${waitTime}" \
+    -v >responceStory.txt ||
+    true #2>/dev/null
 
-curl "${url}/stories/${actualUuid}" \
-  -o tempStory.json \
-  -w "%{http_code}" > responce.txt \
-  -v || true #2>/dev/null
+  sleep "${waitTime}"s
 
-responseStatus=$(<responce.txt)
-if [ "${responseStatus}" = "200" ]
-then
-  printf "\nGET 'story1' SUCCESS!\n"
-  echo 'Object from GET:'
-  echo "$(<tempStory.json)"
-else
-  echo 'TEST GET story1 FAILED'
-  exit 1
-fi
+  responseStatus=$(<responceStory.txt)
+  if [ "${failAttempt}" -gt 5 ]; then
+    echo "GET story1 FAILED after ${failAttempt} !"
+    exit 1
+  fi
+  failAttempt=$(("${failAttempt}" + 1))
+done
+
+echo "GET story1 SUCCESS after ${failAttempt} !"
+printf "\nobject from GET: %s\n" "$(<tempStory.json)"
